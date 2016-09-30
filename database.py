8c10ed919943f941e66bee4cd32fdf31c55d29c3
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2016-09-30 17:06:36 +0200

    Allow to da events

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 
 from threading import Lock
+from includes import Event, User, Order
 import sqlite3
 
 lock = Lock()
@@ -22,40 +23,45 @@ def setup_db():
 def add_user(user):
   lock.acquire()
   try:
-    c.execute("INSERT INTO users values (?,?,?);", user.uid,user.name,user.nick)
+    c.execute("INSERT INTO users values (?,?,?);", (user.uid,user.name,user.nick))
   finally:
+    conn.commit()
     lock.release()
 
 
 def add_event(event):
   lock.acquire()
   try:
-    c.execute("INSERT INTO events values (?,?,?);", event.eid,event.name,event.archive)
+    c.execute("INSERT INTO events values (?,?,?);", (event.eid,event.name,event.archive))
   finally:
+    conn.commit()
     lock.release()
 
 
 def add_order(order):
   lock.acquire()
   try:
-    c.execute("INSERT INTO orders values (?,?,?);", order.uid,order.eid, order.data)
+    c.execute("INSERT INTO orders values (?,?,?);", (order.uid,order.eid, order.data))
   finally:
+    conn.commit()
     lock.release()
 
 
 def del_order(eid, uid):
   lock.acquire()
   try:
-    c.execute("DELETE FROM orders WHERE eid = ? AND uid = ?;", eid, uid)
+    c.execute("DELETE FROM orders WHERE eid = ? AND uid = ?;", (eid, uid))
   finally:
+    conn.commit()
     lock.release()
 
 
 def archive_event(eid, archive=True):
   lock.acquire()
   try:
-    c.execute("UPDATE events SET archive=? WHERE eid=?;", 1 if archive else 0, eid)
+    c.execute("UPDATE events SET archive=? WHERE eid=?;", (1 if archive else 0, eid))
   finally:
+    conn.commit()
     lock.release()
 
 
@@ -85,7 +91,7 @@ def list_orders(eid):
   out = {}
   lock.acquire()
   try:
-    for row in c.execute("SELECT orders.eid, orders.uid, orders.data, users.name, users.nick FROM orders JOIN users on orders.uid = users.uid WHERE orders.eid = ?;", eid):
+    for row in c.execute("SELECT orders.eid, orders.uid, orders.data, users.name, users.nick FROM orders JOIN users on orders.uid = users.uid WHERE orders.eid = ?;", (eid)):
       out[1] = {"user": User(uid=row[1], name=row[3], nick=row[4]), "Order": Order(eid=row[0],uid=row[1],data=[2])}
   finally:
     lock.release()
@@ -94,7 +100,7 @@ def list_orders(eid):
 def get_event(eid):
   lock.acquire()
   try:
-    row = c.execute("SELECT eid, name, archive FROM events WHERE eid = ?;", eid).fetchone()
+    row = c.execute("SELECT eid, name, archive FROM events WHERE eid = ?;", (eid)).fetchone()
     if row is None:
       return None
   finally:
@@ -105,7 +111,7 @@ def get_event(eid):
 def get_user(uid):
   lock.acquire()
   try:
-    row = c.execute("SELECT uid, name, nick FROM users WHERE uid = ?;", uid).fetchone()
+    row = c.execute("SELECT uid, name, nick FROM users WHERE uid = ?;", (uid)).fetchone()
     if row is None:
       return None
   finally:
@@ -115,7 +121,7 @@ def get_user(uid):
 def get_order(uid, eid):
   lock.acquire()
   try:
-    row = c.execute("SELECT eid, uid, data FROM orders WHERE uid = ? AND eid = ?;", uid, eid).fetchone()
+    row = c.execute("SELECT eid, uid, data FROM orders WHERE uid = ? AND eid = ?;", (uid, eid)).fetchone()
     if row is None:
       return None
   finally:
--- handlers.py
+++ handlers.py
@@ -1,5 +1,15 @@
-from telegram.ext import CommandHandler, ConversationHandler
-from database import list_events
+from telegram.ext import MessageHandler, CommandHandler, ConversationHandler, Filters, RegexHandler
+from telegram import ReplyKeyboardMarkup
+from database import list_events, add_event, get_event
+from includes import Event, User, Order
+
+ConvStore = {}
+
+NEW_NAME, NEW_CONFIRM = range(2)
+
+def cancel(bot, update):
+  bot.sendMessage(chat_id=update.message.chat_id, text="Mission aborted!")
+  return ConversationHandler.END
 
 def start(bot, update):
   bot.sendMessage(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
@@ -7,9 +17,48 @@ def start(bot, update):
 
 def events(bot, update):
   events = list_events()
-  bot.sendMessage(chat_id=update.message.chat_id, text="Event List:\n%s" % "\n".join(get_events()) if len(events) > 0 else "No events")
+  bot.sendMessage(chat_id=update.message.chat_id, text="Event List:\n%s" % ("\n".join([str(e) for e in events]) if len(events) > 0 else "No events"))
+
+
+def new_event(bot, update):
+  update.message.reply_text("Please select the name for the new event or type /cancel to abort")
+  return NEW_NAME
+
+def new_event_name(bot, update):
+  name = update.message.text
+  if len(name) < 3:
+    update.message.reply_text(text="Invalid name, must be at least 3 chars")
+    return NEW_NAME
+  ConvStore[update.message.from_user.id] = name
+  reply_keyboard = [['YES', 'NO']]
+  update.message.reply_text("The event name will be: %s" % name, reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+  return NEW_CONFIRM
+
+def new_event_confirm(bot, update):
+  if update.message.text != "YES":
+    return NEW_NAME
+  name = ConvStore.pop(update.message.from_user.id, None)
+  if name is None:
+    return NEW_NAME
+
+  add_event(Event(eid=None, name=name, archive=0))
+  update.message.reply_text("Event with name: \"%s\" has been created" % name)
+  return ConversationHandler.END
 
 start_handler = CommandHandler('start', start)
 event_handler = CommandHandler('events', events)
+
+#add_event_handler = CommandHandler('new', add_event, pass_args=True)
+add_event_handler = ConversationHandler(
+        entry_points=[CommandHandler('new', new_event)],
+
+        states={
+            NEW_NAME: [MessageHandler([Filters.text], new_event_name)],
+
+            NEW_CONFIRM: [RegexHandler('^(YES|NO)$', new_event_confirm)],
+        },
+
+        fallbacks=[CommandHandler('cancel', cancel)]
+);
 
 
